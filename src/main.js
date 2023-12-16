aaffa7080d490406d9a02d4de2294241f68eac13
Idle <651462+reonZ@users.noreply.github.com>
2023-12-16 15:33:46 +0100

    typo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 Hooks.once('ready', () => {
     if (!game.user.isGM) return
     ui.notifications.error(
-        `The module <strong>PF2e Automatic Rune Porgression</strong> is not compatible with the latest versions of the <strong>Pathfinder Second Edition</strong> system, if you still want to use this variant, you can find its replacement in the module <strong>PF2e Toolbelt</strong>.`,
+        `The module <strong>PF2e Automatic Rune Progression</strong> is not compatible with the latest versions of the <strong>Pathfinder Second Edition</strong> system, if you still want to use this variant, you can find its replacement in the module <strong>PF2e Toolbelt</strong>.`,
         { permanent: true }
     )
 })
