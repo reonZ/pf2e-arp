a82df63f536e45eb93ba5b32d8be6268fbf28201
Idle <651462+reonZ@users.noreply.github.com>
2023-12-16 15:34:14 +0100

    1.8.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,2 @@
-(()=>{Hooks.once("ready",()=>{game.user.isGM&&ui.notifications.error("The module <strong>PF2e Automatic Rune Porgression</strong> is not compatible with the latest versions of the <strong>Pathfinder Second Edition</strong> system, if you still want to use this variant, you can find its replacement in the module <strong>PF2e Toolbelt</strong>.",{permanent:!0})});})();
+(()=>{Hooks.once("ready",()=>{game.user.isGM&&ui.notifications.error("The module <strong>PF2e Automatic Rune Progression</strong> is not compatible with the latest versions of the <strong>Pathfinder Second Edition</strong> system, if you still want to use this variant, you can find its replacement in the module <strong>PF2e Toolbelt</strong>.",{permanent:!0})});})();
 //# sourceMappingURL=main.js.map
